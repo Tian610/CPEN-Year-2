18b885655ef6544d20115045affa2109822b02c9
Tian610 <86389601+Tian610@users.noreply.github.com>
2024-09-10 23:25:37 -0700

    i hate this

--- CPEN 221/Labs/Lab 1/Main.java
+++ CPEN 221/Labs/Lab 1/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import additionClosure.AdditionClosure;
 import stringchopping.StringChopping;
 
 public class Main {
@@ -14,12 +19,19 @@ public class Main {
 
         // System.out.println("r = " + WirelessSignalStrength.rss(s,distance,x,a));
 
-        String s = "ONION";
+        // String s = "ONION";
 
-        String[] x = StringChopping.allPossibleDicings(s);
+        // String[] x = StringChopping.allPossibleDicings(s);
 
-        for(int i = 0; i<x.length; i++) {
-            System.out.println(x[i]);
-        }
+        // for(int i = 0; i<x.length; i++) {
+        //     System.out.println(x[i]);
+        // }
+
+        List<Integer> intList = new ArrayList<Integer>();
+        Collections.addAll(intList = new ArrayList<Integer>(), 0,1,100,50,50);
+        int n = 100;
+
+        boolean a = AdditionClosure.isClosed(intList, n);
+        System.out.println(a);
     }
 }
